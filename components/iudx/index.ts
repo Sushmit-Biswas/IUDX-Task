f9e6faba5399f6bee5ca05d6828adf5a33ace9dd
Sushmit Biswas <sushmit.biswas@iiitb.ac.in>
2025-12-21 14:00:00 +0530

    chore: Clean up index exports

--- components/iudx/index.ts
+++ components/iudx/index.ts
@@ -2,8 +2,7 @@ export * from './StatusBadge';
 export * from './ConnectionHeader';
 export * from './TabSelector';
 export * from './ObligationsList';
-// Table is deprecated/removed
-// export * from './ObligationsTable'; 
+
 export * from './ResourceModal';
 export * from './ConsentModal';
 export * from './ActionBar';
